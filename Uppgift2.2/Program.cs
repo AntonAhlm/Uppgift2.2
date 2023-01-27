80a8775d55a09180e47f368276efe3cc17a923ce
Anton <>
2023-01-27 08:32:10 +0100

    Add project files.

---
+++ Uppgift2.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hur många meter hoppade Elin? ");
+            double Elin = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Hur många meter hoppade Alma? ");
+            double Alma = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Elin hoppade " + (Elin - Alma) + " meter länrge än Alma");  
+
+        }
+    }
+}
